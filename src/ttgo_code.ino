349ca067d7befff20f7de32d2dbf27241e742f28
VitRuzicka <36342077+VitRuzicka@users.noreply.github.com>
2021-02-26 19:17:19 +0100

    Update ttgo_code.ino

--- src/ttgo_code.ino
+++ src/ttgo_code.ino
@@ -23,7 +23,42 @@
 
 TFT_eSPI tft = TFT_eSPI(135, 240); // Invoke custom library
 
+
+void espDelay(int ms)
+{
+    esp_sleep_enable_timer_wakeup(ms * 1000);
+    esp_sleep_pd_config(ESP_PD_DOMAIN_RTC_PERIPH, ESP_PD_OPTION_ON);
+    esp_light_sleep_start();
+}
+
+
 void setup(){
+  //VYSTUPY
+  pinMode(BUTTON_1, INPUT);
+  
+  
+  
+  
+  //DISPLAY
+  tft.init();
+    tft.setRotation(2);
+    tft.fillScreen(TFT_BLACK);
+    tft.setTextSize(2);
+    tft.setTextColor(TFT_GREEN);
+    tft.setCursor(0, 0);
+    tft.setTextDatum(MC_DATUM);
+    tft.setTextSize(1);
+
+    /*
+    if (TFT_BL > 0) {                           // TFT_BL has been set in the TFT_eSPI library in the User Setup file TTGO_T_Display.h
+        pinMode(TFT_BL, OUTPUT);                // Set backlight pin to output mode
+        digitalWrite(TFT_BL, TFT_BACKLIGHT_ON); // Turn backlight on. TFT_BACKLIGHT_ON has been set in the TFT_eSPI library in the User Setup file TTGO_T_Display.h
+    }
+    */
+
+    tft.setSwapBytes(true);
+    tft.pushImage(0, 0,  240, 135, ttgo);
+    espDelay(5000);
 }
 void loop(){
 }
