96945a21d9e8f92e6ed253859d757ae390d72cfb
Vit Ruzicka <vit.vitek@gmail.com>
2021-02-27 21:03:01 +0100

    testy displeje

--- src/src.ino
+++ src/src.ino
@@ -17,48 +17,97 @@
 
 
 #define ADC_EN              14  //ADC_EN is the ADC detection enable port
-#define ADC_PIN             34
+#define ADC_BATT             34
 #define BUTTON_1            35
 #define BUTTON_2            0
+int vref = 1100;
+
+
 
 TFT_eSPI tft = TFT_eSPI(135, 240); // Invoke custom library
+Button2 btn1(BUTTON_1);
+Button2 btn2(BUTTON_2);
 
 
 void espDelay(int ms)
 {
-    esp_sleep_enable_timer_wakeup(ms * 1000);
-    esp_sleep_pd_config(ESP_PD_DOMAIN_RTC_PERIPH, ESP_PD_OPTION_ON);
-    esp_light_sleep_start();
+  esp_sleep_enable_timer_wakeup(ms * 1000);
+  esp_sleep_pd_config(ESP_PD_DOMAIN_RTC_PERIPH, ESP_PD_OPTION_ON);
+  esp_light_sleep_start();
+}
+
+void showMenu(){
+  static uint64_t timeStamp = 0;
+  if (millis() - timeStamp > 1000) { //refreah rate kazduch 1000 ms
+    timeStamp = millis();
+    tft.fillScreen(TFT_BLACK);
+    uint16_t v = analogRead(ADC_BATT);
+    float battery_voltage = ((float)v / 4095.0) * 2.0 * 3.3 * (vref / 1000.0) - 0.2;
+    tft.drawString(String(battery_voltage) + "V", tft.width(), 0);
+
+  }
+  
+}
+
+void button_init()
+{
+    btn1.setLongClickHandler([](Button2 & b) {
+        btnCick = false;
+        int r = digitalRead(TFT_BL);
+        tft.fillScreen(TFT_BLACK);
+        tft.setTextColor(TFT_GREEN, TFT_BLACK);
+        tft.setTextDatum(MC_DATUM);
+        tft.drawString("Press again to wake up",  tft.width() / 2, tft.height() / 2 );
+        espDelay(6000);
+        digitalWrite(TFT_BL, !r);
+
+        tft.writecommand(TFT_DISPOFF);
+        tft.writecommand(TFT_SLPIN);
+        //After using light sleep, you need to disable timer wake, because here use external IO port to wake up
+        esp_sleep_disable_wakeup_source(ESP_SLEEP_WAKEUP_TIMER);
+        // esp_sleep_enable_ext1_wakeup(GPIO_SEL_35, ESP_EXT1_WAKEUP_ALL_LOW);
+        esp_sleep_enable_ext0_wakeup(GPIO_NUM_35, 0);
+        delay(200);
+        esp_deep_sleep_start();
+    });
+    btn1.setPressedHandler([](Button2 & b) {
+        Serial.println("Detect Voltage..");
+        btnCick = true;
+    });
+
+    btn2.setPressedHandler([](Button2 & b) {
+        btnCick = false;
+        Serial.println("btn press wifi scan");
+        wifi_scan();
+    });
 }
 
 
-void setup(){
+
+void setup() {
   //VYSTUPY
   pinMode(BUTTON_1, INPUT);
-  
-  
-  
-  
+
+
+
+
   //DISPLAY
   tft.init();
-    tft.setRotation(2);
-    tft.fillScreen(TFT_BLACK);
-    tft.setTextSize(2);
-    tft.setTextColor(TFT_GREEN);
-    tft.setCursor(0, 0);
-    tft.setTextDatum(MC_DATUM);
-    tft.setTextSize(1);
+  tft.setRotation(2);
+  tft.fillScreen(TFT_BLACK);
+  tft.setTextColor(TFT_GREEN);
+  tft.setCursor(0, 0);
+  tft.setTextDatum(TR_DATUM);
+  tft.setTextSize(2);
+  tft.setSwapBytes(true);
+  tft.pushImage(0, 0,  135, 240, logo);
+  espDelay(5000);
+  tft.fillScreen(TFT_BLACK);
+  uint16_t v = analogRead(ADC_BATT);
+  float battery_voltage = ((float)v / 4095.0) * 2.0 * 3.3 * (vref / 1000.0);
+  tft.drawString(String(battery_voltage) + "V", tft.width(), 0);
 
-    /*
-    if (TFT_BL > 0) {                           // TFT_BL has been set in the TFT_eSPI library in the User Setup file TTGO_T_Display.h
-        pinMode(TFT_BL, OUTPUT);                // Set backlight pin to output mode
-        digitalWrite(TFT_BL, TFT_BACKLIGHT_ON); // Turn backlight on. TFT_BACKLIGHT_ON has been set in the TFT_eSPI library in the User Setup file TTGO_T_Display.h
-    }
-    */
-
-    tft.setSwapBytes(true);
-    tft.pushImage(0, 0,  240, 135, ttgo);
-    espDelay(5000);
 }
-void loop(){
+void loop() {
+
 }
