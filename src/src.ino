568611ca7f9dfa591aa80b378914dd5b87651f81
VitRuzicka <36342077+VitRuzicka@users.noreply.github.com>
2021-03-01 18:01:45 +0100

    Update src.ino

--- src/src.ino
+++ src/src.ino
@@ -5,18 +5,19 @@
 #include "Button2.h"
 #include "esp_adc_cal.h"
 #include "bmp.h"
+#include <analogWrite.h>
 
 
-// TFT Pins has been set in the TFT_eSPI library in the User Setup file TTGO_T_Display.h
 // #define TFT_MOSI            19
 // #define TFT_SCLK            18
 // #define TFT_CS              5
 // #define TFT_DC              16
 // #define TFT_RST             23
-// #define TFT_BL              4   // Display backlight control pin
+// #define TFT_BL              4   
 
-
-#define ADC_EN              14  //ADC_EN is the ADC detection enable port
+#define JAS                 50
+#define CONNECTED             1  //predelat az se esp pripoji pres esp-now
+#define ADC_EN              14  
 #define ADC_BATT             34
 #define BUTTON_1            35
 #define BUTTON_2            0
@@ -25,8 +26,7 @@ int vref = 1100;
 
 
 TFT_eSPI tft = TFT_eSPI(135, 240); // Invoke custom library
-Button2 btn1(BUTTON_1);
-Button2 btn2(BUTTON_2);
+
 
 
 void espDelay(int ms)
@@ -44,42 +44,10 @@ void showMenu(){
     uint16_t v = analogRead(ADC_BATT);
     float battery_voltage = ((float)v / 4095.0) * 2.0 * 3.3 * (vref / 1000.0) - 0.2;
     tft.drawString(String(battery_voltage) + "V", tft.width(), 0);
-
+    if(CONNECTED){
+  tft.pushImage(0, 0,  20, 15, wifi);
   }
-  
-}
-
-void button_init()
-{
-    btn1.setLongClickHandler([](Button2 & b) {
-        btnCick = false;
-        int r = digitalRead(TFT_BL);
-        tft.fillScreen(TFT_BLACK);
-        tft.setTextColor(TFT_GREEN, TFT_BLACK);
-        tft.setTextDatum(MC_DATUM);
-        tft.drawString("Press again to wake up",  tft.width() / 2, tft.height() / 2 );
-        espDelay(6000);
-        digitalWrite(TFT_BL, !r);
-
-        tft.writecommand(TFT_DISPOFF);
-        tft.writecommand(TFT_SLPIN);
-        //After using light sleep, you need to disable timer wake, because here use external IO port to wake up
-        esp_sleep_disable_wakeup_source(ESP_SLEEP_WAKEUP_TIMER);
-        // esp_sleep_enable_ext1_wakeup(GPIO_SEL_35, ESP_EXT1_WAKEUP_ALL_LOW);
-        esp_sleep_enable_ext0_wakeup(GPIO_NUM_35, 0);
-        delay(200);
-        esp_deep_sleep_start();
-    });
-    btn1.setPressedHandler([](Button2 & b) {
-        Serial.println("Detect Voltage..");
-        btnCick = true;
-    });
-
-    btn2.setPressedHandler([](Button2 & b) {
-        btnCick = false;
-        Serial.println("btn press wifi scan");
-        wifi_scan();
-    });
+  }
 }
 
 
@@ -87,7 +55,7 @@ void button_init()
 void setup() {
   //VYSTUPY
   pinMode(BUTTON_1, INPUT);
-
+  pinMode(4, OUTPUT); //ovladani jasu
 
 
 
@@ -101,13 +69,13 @@ void setup() {
   tft.setTextSize(2);
   tft.setSwapBytes(true);
   tft.pushImage(0, 0,  135, 240, logo);
-  espDelay(5000);
+  espDelay(1000);
+  analogWrite(4, JAS);
   tft.fillScreen(TFT_BLACK);
-  uint16_t v = analogRead(ADC_BATT);
-  float battery_voltage = ((float)v / 4095.0) * 2.0 * 3.3 * (vref / 1000.0);
-  tft.drawString(String(battery_voltage) + "V", tft.width(), 0);
+  
 
 }
 void loop() {
-
+  
+showMenu();
 }
